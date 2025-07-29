c1da1afe26ed4c5a51044387c71cbbc98e9a55af
PScottZero <pauljscott8@gmail.com>
2025-07-28 22:54:11 -0400

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,21 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
 <svelte:head>
-  <title>Royal Game of Ur</title>
+	<title>Royal Game of Ur</title>
 </svelte:head>
 
 <div id="sidebar">
-  <p class="cuneiform">𒊒𒅋 𒂵𒅎 𒄴 𒌨</p>
+	<p class="cuneiform">𒊒𒅋 𒂵𒅎 𒄴 𒌨</p>
 </div>
 
 <div id="board">
-  <img src="/board.svg" alt="Game Board"/>
+	<img src="{base}/board.svg" alt="Game Board" />
 </div>
 
 <style lang="scss">
-  :global {
+	:global {
 		* {
 			box-sizing: border-box;
 			padding: 0;
@@ -24,40 +28,40 @@
 			font-family: Arial, Helvetica, sans-serif;
 		}
 
-    body {
-      display: grid;
-      grid-template-columns: 20rem 1fr;
-      width: 100vw;
-      height: 100vh;
-    }
+		body {
+			display: grid;
+			grid-template-columns: 20rem 1fr;
+			width: 100vw;
+			height: 100vh;
+		}
 
 		a {
 			text-decoration: none;
 		}
 	}
 
-  @font-face {
-    font-family: "CuneiformNA";
-    src: url("/CuneiformNA.ttf");
-  }
+	@font-face {
+		font-family: 'CuneiformNA';
+		src: url('/CuneiformNA.ttf');
+	}
 
-  #sidebar {
-    background-color: #c8ba9f;
-    padding: 1rem;
-  }
+	#sidebar {
+		background-color: #c8ba9f;
+		padding: 1rem;
+	}
 
-  #board {
-    padding: 5%;
+	#board {
+		padding: 5%;
 
-    img {
-      width: 100%;
-      height: 100%;
-    }
-  }
+		img {
+			width: 100%;
+			height: 100%;
+		}
+	}
 
-  .cuneiform {
-    font-family: 'CuneiformNA';
-    font-size: 20px;
-    font-weight: bold;
-  }
+	.cuneiform {
+		font-family: 'CuneiformNA';
+		font-size: 20px;
+		font-weight: bold;
+	}
 </style>
